659238a93f3c155f5af0b42ce92d74fb24d3f94e
rodrigo schmitz <rodri.schmitz@gmail.com>
2026-08-05 13:39:25 +0000

    agrego modelos staging de sucursales y comercio

---
+++ models/example/staging/stg_comercio.sql
@@ -0,0 +1,7 @@
+SELECT
+    id_comercio,
+    id_bandera,
+    comercio_cuit AS cuit,
+    comercio_razon_social AS razon_social,
+    comercio_bandera_nombre AS bandera_nombre
+FROM {{ source('sepa', 'comercio') }}
---
+++ models/example/staging/stg_sucursales.sql
@@ -0,0 +1,15 @@
+SELECT
+    id_comercio,
+    id_bandera,
+    id_sucursal,
+    sucursales_nombre AS nombre_sucursal,
+    sucursales_tipo AS tipo_sucursal,
+    sucursales_calle AS calle,
+    sucursales_numero AS numero,
+    sucursales_barrio AS barrio,
+    sucursales_localidad AS localidad,
+    sucursales_provincia AS provincia,
+    sucursales_codigo_postal AS codigo_postal,
+    CAST(sucursales_latitud AS FLOAT64) AS latitud,
+    CAST(sucursales_longitud AS FLOAT64) AS longitud
+FROM {{ source('sepa', 'sucursales') }}
